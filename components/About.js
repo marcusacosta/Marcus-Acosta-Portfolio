6d44aad497aabc0c3d6c5e231ecb55f4ba445f73
Marcus Acosta <marcarti@mac.attlocal.net>
2025-06-12 22:59:37 -0700

    edited

--- components/About.js
+++ components/About.js
@@ -3,7 +3,7 @@ export default function About() {
     <section id="about">
       <div className="center">
         <h2>About</h2>
-          <p>I'm a Computer Science student at Oregon State University with a passion for building scalable applications, developing robust APIs, and working with modern frontend and backend technologies.</p>
+          <p>I&apos;m a Computer Science student at Oregon State University with a strong interest in building powerful, real-world applications using modern technologies.</p>
       </div>
     </section>
   );
