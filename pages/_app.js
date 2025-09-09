497935dc4d7c215e83ea0b085254f168d13c2e66
marcusacosta <marcarti@mac.attlocal.net>
2025-09-09 11:45:42 -0700

    Upgrade Next.js to v14 and add Vercel Analytics
    - Upgraded Next.js from 12.3.1 to 14.2.32
    - Updated React to 18.2.0 and ESLint config
    - Added @vercel/analytics for website analytics
    - All builds passing successfully

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/home.scss'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 export default MyApp
